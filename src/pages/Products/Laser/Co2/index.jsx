66aebb2881dafb98a8d5ab67dc1d30e647307d68
hiren2603 <secure.hiren@gmail.com>
2023-06-11 10:21:37 +0530

    merge incoming changes in co2

--- src/pages/Products/Laser/Co2/index.jsx
+++ src/pages/Products/Laser/Co2/index.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import LaserImg from "../../../../assets/products/co2.png";
 import Samples from "../../../../assets/samples/co2_samples.png";
 import Heading from "../../../../components/Heading";
-import { Grid, Typography, List, Card } from "@mui/material";
 import {
   productHeading,
   modelHeading,
@@ -17,7 +16,7 @@ import {
   benifits,
   commonSpace,
 } from "../../../../helpers/co2Data";
-import { ProductImg, headings, modelHeading, productHeading } from "./style";
+// import { ProductImg, headings, modelHeading, productHeading } from "./style";
 
 const Co2 = () => {
   return (
@@ -49,47 +48,47 @@ const Co2 = () => {
                 <Card key={item.id} elevation={7} sx={{ p: "1rem" }}>
                   <Typography sx={modelHeading}>{item.model}</Typography>
 
-                    <Grid item container alignItems={{ lg: "center" }}>
-                      <Grid item lg={5} xs={6}>
-                        <Typography sx={headings}>
-                          {item.power.heading}:
-                        </Typography>
-                      </Grid>
-                      <Grid item>{item.power.content}</Grid>
+                  <Grid item container alignItems={{ lg: "center" }}>
+                    <Grid item lg={5} xs={6}>
+                      <Typography sx={headings}>
+                        {item.power.heading}:
+                      </Typography>
                     </Grid>
+                    <Grid item>{item.power.content}</Grid>
+                  </Grid>
 
-                    <Grid item container alignItems={"center"}>
-                      <Grid item lg={5} xs={6}>
-                        <Typography sx={headings}>
-                          {item.mark_speed.heading}:
-                        </Typography>
-                      </Grid>
-                      <Grid item>{item.mark_speed.content}</Grid>
+                  <Grid item container alignItems={"center"}>
+                    <Grid item lg={5} xs={6}>
+                      <Typography sx={headings}>
+                        {item.mark_speed.heading}:
+                      </Typography>
                     </Grid>
+                    <Grid item>{item.mark_speed.content}</Grid>
+                  </Grid>
 
-                    <Grid item container alignItems={"center"}>
-                      <Grid item lg={5} xs={6}>
-                        <Typography sx={headings}>
-                          {item.mark_area.heading}:
-                        </Typography>
-                      </Grid>
-                      <Grid item>{item.mark_area.content}</Grid>
+                  <Grid item container alignItems={"center"}>
+                    <Grid item lg={5} xs={6}>
+                      <Typography sx={headings}>
+                        {item.mark_area.heading}:
+                      </Typography>
                     </Grid>
+                    <Grid item>{item.mark_area.content}</Grid>
+                  </Grid>
 
-                    <Grid item container alignItems={"center"}>
-                      <Grid item lg={5} xs={6}>
-                        <Typography sx={headings}>
-                          {item.machine_speed.heading}:
-                        </Typography>
-                      </Grid>
-                      <Grid item lg={5} xs={6}>
-                        {item.machine_speed.content}
-                      </Grid>
+                  <Grid item container alignItems={"center"}>
+                    <Grid item lg={5} xs={6}>
+                      <Typography sx={headings}>
+                        {item.machine_speed.heading}:
+                      </Typography>
                     </Grid>
-                  </Card>
-                </Grid>
-              );
-            })}
+                    <Grid item lg={5} xs={6}>
+                      {item.machine_speed.content}
+                    </Grid>
+                  </Grid>
+                </Card>
+              </Grid>
+            );
+          })}
 
           {/* Specification */}
           <Grid
@@ -130,35 +129,34 @@ const Co2 = () => {
           </Grid>
         </Grid>
 
-          {/* Print Samples, Image and Applicable Products */}
-          <Grid item container direction={"column"} lg={4} rowSpacing={4}>
-            <Grid item container direction={"column"} xs={1}>
-              <img src={LaserImg} alt="Laser" width={300} />
-            </Grid>
+        {/* Print Samples, Image and Applicable Products */}
+        <Grid item container direction={"column"} lg={4} rowSpacing={4}>
+          <Grid item container direction={"column"} xs={1}>
+            <img src={LaserImg} alt="Laser" width={300} />
+          </Grid>
 
-            <Grid item xs={12}>
-              <Typography sx={modelHeading}>Print Samples</Typography>
-              <ProductImg src={Samples} />
-            </Grid>
+          <Grid item xs={12}>
+            <Typography sx={modelHeading}>Print Samples</Typography>
+            <ProductImg src={Samples} />
+          </Grid>
 
-            <Grid item>
-              <Typography sx={modelHeading}>Applicable Products</Typography>
-              {applications?.map((item, index) => {
-                return <List key={index}>{item}</List>;
-              })}
-            </Grid>
+          <Grid item>
+            <Typography sx={modelHeading}>Applicable Products</Typography>
+            {applications?.map((item, index) => {
+              return <List key={index}>{item}</List>;
+            })}
           </Grid>
         </Grid>
-
-        {/* Benifits */}
-        <Grid item lg>
-          <Typography sx={modelHeading}>Benifits</Typography>
-          <List>{benifits[0].marking}</List>
-          <List>{benifits[0].efficiency}</List>
-          <List>{benifits[0].power}</List>
-        </Grid>
       </Grid>
-    </>
+
+      {/* Benifits */}
+      <Grid item lg>
+        <Typography sx={modelHeading}>Benifits</Typography>
+        <List>{benifits[0].marking}</List>
+        <List>{benifits[0].efficiency}</List>
+        <List>{benifits[0].power}</List>
+      </Grid>
+    </Grid>
   );
 };
 
