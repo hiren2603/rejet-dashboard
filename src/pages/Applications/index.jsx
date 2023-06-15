e70da2c2e357c72da43bb5ae94a719621af07ab3
28Meet <90207433+28Meet@users.noreply.github.com>
2023-06-15 16:06:46 +0530

    Work on application page

--- src/pages/Applications/index.jsx
+++ src/pages/Applications/index.jsx
@@ -1,8 +1,10 @@
 import Head from "@/components/Head";
-import React from "react";
+import { Grid, Typography } from "@mui/material";
+import { videoData } from "helpers/videoData";
+import YouTube from "react-youtube";
+import { productHeading } from "./style";
 
 const Applications = () => {
-  return <div>Applications</div>;
   const opts = {
     height: "250",
     width: "300",
@@ -10,29 +12,41 @@ const Applications = () => {
   };
   return (
     <>
-      <Grid container>
-        <Typography>
-          <Head
-            title="Applications"
-            description="Radhe Enterprise Application Page"
-          />
-          Applications of RE200 Inkjet Printers
-        </Typography>
-        {videoData?.map((item) => {
-          return (
-            <Grid item key={item.id}>
-              <ReactPlayer url={item.url} />
-            </Grid>
-          );
-        })}
-        {/* {videoData?.map((item) => {
-          var id = getYouTubeID(item.sourceId);
-          return (
-            <Grid item key={item.id}>
-              <YouTube videoId={id} opts={opts} />
-            </Grid>
-          );
-        })} */}
+      <Grid
+        container
+        alignItems="center"
+        justifyContent={"center"}
+        sx={{ width: "80%", margin: "auto" }}
+      >
+        <Grid item lg>
+          <Typography textAlign={"center"} sx={productHeading}>
+            <Head
+              title="Applications"
+              description="Radhe Enterprise Application Page"
+            />
+            Applications of RE200 Inkjet Printers
+          </Typography>
+        </Grid>
+
+        <Grid item container columns={3}>
+          {videoData?.map((item) => {
+            // var id = getYouTubeID(item.sourceId);
+            return (
+              <Grid
+                item
+                key={item.id}
+                lg={1}
+                sx={{ alignSelf: "center", borderRadius: "3px" }}
+              >
+                <YouTube
+                  videoId={item.sourceId}
+                  opts={opts}
+                  style={{ borderRadius: "20px" }}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
       </Grid>
     </>
   );
