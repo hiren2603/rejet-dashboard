db668e844c849ac71af552a1d197abb2e3e5a29b
hiren2603 <secure.hiren@gmail.com>
2023-06-15 14:31:48 +0530

    contact page 2 section completed

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,12 +1,47 @@
 import Head from "@/components/Head";
-import React from "react";
+import { Divider, Grid, Typography } from "@mui/material";
+import { sectionHeadingStyle } from "./style";
+import PageHeading from "@/components/PageHeading";
+import { HO, BRANCHES } from "helpers/contact";
 
 const Contact = () => {
   return (
-    <div>
+    <Grid container sx={{ width: "90%", mx: "auto", mt: 5 }}>
       <Head title="Contact" description="Radhe Enterprise Contact Page" />
-      Contact
-    </div>
+      <PageHeading heading="Contact Us" />
+      <Grid item my={2} md={7}>
+        <Typography variant="h5" sx={sectionHeadingStyle}>
+          Locations
+        </Typography>
+        <Divider sx={{ mr: "2rem" }} />
+      </Grid>
+      <Grid item md={5} my={2}>
+        <Typography variant="h5" sx={sectionHeadingStyle}>
+          Inquiry
+        </Typography>
+        <Divider />
+      </Grid>
+      <Grid item md={5}>
+        <Typography variant="h5" sx={sectionHeadingStyle} mb={2}>
+          {HO.head}
+        </Typography>
+        <img src={HO.image} alt="company_logo" />
+        <Typography fontWeight={600} lineHeight={2}>
+          {HO.address}
+        </Typography>
+      </Grid>
+      <Grid item md={5}>
+        <Typography variant="h5" sx={sectionHeadingStyle} mb={2}>
+          {BRANCHES.head}
+        </Typography>
+        {BRANCHES.branchList.map((branch) => (
+          <Typography fontWeight={600} lineHeight={2} key={branch.id}>
+            {branch.name}
+          </Typography>
+        ))}
+      </Grid>
+      <Grid item md={5}></Grid>
+    </Grid>
   );
 };
 
---
+++ src/pages/Contact/style.js
@@ -0,0 +1,18 @@
+export const sectionHeadingStyle = {
+  color: "#5b5b5b",
+  textShadow: "2px 2px 2px #0000004f",
+  fontWeight: "600",
+  textTransform: "uppercase",
+  fontSize: "30px",
+  position: "relative",
+  // "&:after": {
+  //   content: '" "',
+  //   position: "absolute",
+  //   bottom: 0,
+  //   left: "0%",
+  //   width: "2rem",
+  //   height: "3px",
+  //   backgroundColor: "#5b5b5b",
+  //   borderRadius: "5px",
+  // },
+};
