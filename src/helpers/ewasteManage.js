4ec104a09a4aa3cfb9f7f193de9b228ceabad6c9
28Meet <90207433+28Meet@users.noreply.github.com>
2023-06-21 15:42:53 +0530

    Make ewaste management page

---
+++ src/helpers/ewasteManage.js
@@ -0,0 +1,296 @@
+export const ewasteRules = [
+  "E-waste is a popular, informal name for electronic products nearing the end of their useful life. E-wastes are considered dangerous, as certain components of some electronic products contain materials that are hazardous, depending on their condition and density. The hazardous content of these materials pose a threat to human health and environment. Discarded computers, refrigerators, washing machines, televisions, VCRs, stereos, copiers, fax machines, electric lamps, cell phones, audio equipment , other Electronic Items and batteries if improperly disposed can leach lead and other substances into soil and groundwater. Many of these products can be reused, refurbished, or recycled in an environmentally sound manner so that they are less harmful to the ecosystem.",
+];
+
+export const benefits = [
+  {
+    id: 1,
+    title:
+      "Electronic recycling helps protect public health and the environment",
+    content:
+      "Many electronics have toxic or hazardous materials such as mercury and lead, which can be harmful to the environment if disposed of in trashcans. Reusing and recycling electronics safely helps in keeping the hazardous materials from harming humans or the environment. For example, televisions and computer monitors are hazardous since they have lead in them. Printed circuit boards contain harmful materials such as cadmium, lead, mercury and chromium. Also, batteries in computers and other electronics may contain hazardous materials such as cadmium, mercury and lead. Instead of keeping old electronics in the house or dumping them in landfills, recycling or reusing them is an appropriate option that should be supported by individuals and organizations. Considering the benefits of electronics recycling, it is very important that people in various parts around the world embrace this concept.",
+  },
+  {
+    id: 2,
+    title: "Electronics recycling conserves natural resources",
+    content:
+      "There are many materials that can be recovered from old electronics. These materials can be used to make new products, thus reducing the need to mine for new raw materials. For instance, various metals can be recovered from computer circuit board, sand other electronics, and the plastics and glass found in computer monitors and televisions can be recycled",
+  },
+  {
+    id: 3,
+    title: "Electronics recycling supports the community",
+    content:
+      "Donating your old electronics plays an important role in the provision of refurbished products such as computers and mobile phones, which can be of great help to low-income families, schools, and not-for-profit organizations. It also helps individuals gain access to technology that they could not have other-wise afforded.",
+  },
+  {
+    id: 4,
+    title: "Electronics recycling creates employment locally",
+    content:
+      "Considering that around90 percent of electronic equipment is recyclable, electronics recycling can play a significant role in creating employment. This is because new firms dealing with electronics recycling will form and existing firms will look to employ more people to recover recyclable materials. This can be triggered by the increase in the demand for electronics recycling.",
+  },
+  {
+    id: 5,
+    title: "Creates jobs",
+    content:
+      "E-waste recycling creates new jobs for professional recyclers and creates a second market for the recycled materials",
+  },
+];
+
+export const dos = [
+  "Always look for information on the catalogue with your product for end-of-life equipment handling.",
+  "Ensure that only Authorized Recyclers/Dismantler handle your electronic (i.e. LED TV’s and accessories)products",
+  "Always call at our toll-free Numbers to Dispose products that have reached end-of life",
+  "Always drop your used electronic products, batteries or any accessories when they reach the end of their life at your nearest Authorized E-Waste Collection Points.",
+  "Always disconnect the battery from product, and ensure any glass surface is protected against breakage.",
+];
+
+export const donts = [
+  "Do not dismantle your electronic Products on your own",
+  "Do not throw electronics in bins having “Do not Dispose “sign.",
+  "Do not give e-waste to informal and unorganized sectors like Local Scrap Dealer/ Rag Pickers.",
+  "Do not dispose your product in garbage bins along with municipal waste that ultimately reaches landfills.",
+];
+
+export const data = [
+  {
+    index: 1,
+    state: "Andhra Pradesh, Telangana",
+    location: "Vijaywada",
+    address:
+      "Plot No. 232 4th Cross Road, New Auto Nagar Near Atithi Hotel Vijaywada Pin - 520007",
+    contactPerson: "Mr. Vishalnath Tiwari",
+    contactNo: "8886332168",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 2,
+    state:
+      "Assam, Nagaland, Sikkim, Mizoram, Arunachal Pradesh, Meghalaya, Manipur, Tripura",
+    location: "Guwahati",
+    address:
+      "Om Logistics Ltd. M/S Lodhasons Consultancy Services Private Limited Care Of Sri Nripen Das ,Betakuchi, Behind Bhargav Hotal ,Dto Office, Opp- Tirupati Balaji Temple N.H. Way 37, Guwahati -781035, Assam",
+    contactPerson: "Mr Dalbir Poonia",
+    contactNo: "7086049094",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 3,
+    state: "Bihar",
+    location: "Patna",
+    address:
+      "Om Logistics Ltd Near Nh-30 Behind Bharat Petrol Pump Near Patna, Marble Nand Lal Chapra Krishna Niketan School Road, Patna – 800007",
+    contactPerson: "Mr.Vikram Singh",
+    contactNo: "7763801967",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 4,
+    state: "Delhi",
+    location: "Delhi",
+    address:
+      "Palam Gurgaon Road Behind Rajasthan Highway Petrol Pump Kapashera, New Delhi-110037",
+    contactPerson: "Mr.Sanjay Kaushik",
+    contactNo: "9211783457",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 5,
+    state: "Goa",
+    location: "Goa",
+    address:
+      "Plot No.S141,Phase Iiib, Near Hotel Ravi Cafe, Near Lokmat Press, Verna, Goa 403722",
+    contactPerson: "Mr.Arvind Kumar",
+    contactNo: "9881777107",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 6,
+    state: "Gujarat",
+    location: "Ahmedabad",
+    address:
+      "Om Logistics Ltd Survey No 862,/174 Behind Kankavati Hotel Village Rajoda, Sarkhej Bavla Highway Distt.- Ahmedabad Pin -382220",
+    contactPerson: "Mr. Rajkumar Yadav",
+    contactNo: "9714502001",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 7,
+    state: "Gujarat",
+    location: "Baroda",
+    address:
+      "Om Logistics Limited C-1 To 12, Maruti Estate, Nr. Halol Highway Toll Naka, Golden Choukadi, Vadodara.(Guj.) – 390022",
+    contactPerson: "Mr.Vijay Kumar Sharma",
+    contactNo: "9714502021",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 8,
+    state: "Haryana, Chandigarh",
+    location: "Ambala",
+    address:
+      "N.H.1, Opp.- Mirchi Restorent Near Kaushal, Filling Station Sambalkhi Road, Mohri ,Ambala (Haryana) Pin Code – 136135",
+    contactPerson: "Mr.B.D.Sharma",
+    contactNo: "8397970401",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 9,
+    state: "Haryana, Chandigarh",
+    location: "Gurgaon",
+    address:
+      "Om Logistics Ltd. Plot No. – 4, Binola Industrial Area Near Terrysoft, Gurgaon Haryana Pin Code – 122001",
+    contactPerson: "Mr.Anil Yadav",
+    contactNo: "9811121899",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 10,
+    state: "Himachal Pradesh",
+    location: "Solan",
+    address:
+      "Kharsa No. 427/163/2, Vill.Bhatoli Khurd, Near Abbott Healthcare Pvt.Ltd., Birla,Unichem Road, Teh.Baddi Distt.Solan(Hp) Baddi (Hp) Pin Code - 173205",
+    contactPerson: "Mr. Pawan Kumar",
+    contactNo: "7832940021",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 11,
+    state: "Jammu & Kashmir, Ladakh",
+    location: "Jammu",
+    address:
+      "H.N.-6,Ward No.- 4 Rajeev Colany Near Bishna Road Bari Brahmna Opp. Army Training Ground Jammu-181133",
+    contactPerson: "Mr.Sandeep Kumar",
+    contactNo: "8492950730",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 12,
+    state: "Jharkhand",
+    location: "Jamshedpur",
+    address:
+      "Om Logistics Ltd Plot No,18 Bhilai Pahari Highway No, 33 Near Yadav Hotal Jamshedpur Pin -831012",
+    contactPerson: "Mr.S. K. Tiwari",
+    contactNo: "9708037429",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 13,
+    state: "Karnataka",
+    location: "Bangalore",
+    address:
+      "Om Logistics Ltd Nh4 Pune To Bangalore Highway, Opposite Of Indian Petrol Bank, Dabbaspet .Bangalore-562111 Karnataka",
+    contactPerson: "Mr. Pradeep Chauhan",
+    contactNo: "9686860094",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 14,
+    state: "Kerala, Lakshadweep",
+    location: "Cochin",
+    address:
+      "Om Logistics Ltd. Building Numbers & No. Xv/180 & Xv/180a, Karumalloor Panchayat U.C College, Aluva (P.O), Ernakulam, Kerala-683102",
+    contactPerson: "Mr.Gyanendra Kumar",
+    contactNo: "7034025581",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 15,
+    state: "Madhya Pradesh, Chhattisgarh",
+    location: "Bhopal",
+    address:
+      "Plot No-45, Near Ganesh Puri Railway Crossing, Hoshangabad Road, Bhopal -462024 (M.P.)",
+    contactPerson: "Mr.Sandeep Pandey",
+    contactNo: "9993125874",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 16,
+    state: "Maharashtra",
+    location: "Mumbai",
+    address:
+      "Om Logistics Ltd Shop No. 4, Om Datta Prasad Society, Sahar Road, Chakala, Andheri (East), Mumbai - 400099",
+    contactPerson: "Mr.Vikram Dabolkar",
+    contactNo: "9930302372",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 17,
+    state: "Maharashtra",
+    location: "Pune",
+    address:
+      "Om Logistics Ltd M/S.Pasalkar Bhavan Plot No.31, Flat No.3, 1st Floor, Keshav Nagar, Nr.Sheetal Petrol Pump,Chinchwad, Pune - 411033",
+    contactPerson: "Mr. Vikram Dabolkar",
+    contactNo: "9930302372",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 18,
+    state: "Orissa",
+    location: "Bhubaneswar",
+    address:
+      "Om Logistics Ltd Plot No: 357/1331, Madhusudan Nagar,Near Toshali Resort,Unit -4, Bhubaneswar- 751001,Orissa",
+    contactPerson: "Mr.Sachin Rana",
+    contactNo: "7064410142",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 19,
+    state: "Punjab, Himachal Pradesh",
+    location: "Ludhiana",
+    address:
+      "Om Logistics Ltd Plot No-24 Transport Nagar, Near God Gift, Ludhiana-141003",
+    contactPerson: "Mr.Girish Kumar",
+    contactNo: "9878404576",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 20,
+    state: "Rajasthan",
+    location: "Jaipur",
+    address:
+      "Om Logistics Ltd Plot No. – B-1101, Road No. 9 F3, V.K.I Area, Jaipur (Raj.) - 302013",
+    contactPerson: "Mr. Rajkumar Yadav",
+    contactNo: "9714502001",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 21,
+    state: "Tamil Nadu, Puducherry",
+    location: "Chennai",
+    address:
+      "Om Logistics Ltd. No. 2, Sai Nagar, Off. Kodungaiyur High Road, Near Madhavaram Bridge, Madhavaram, Chennai - 600060",
+    contactPerson: "Mr.Rajan Sharma",
+    contactNo: "7299959714",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 22,
+    state: "Uttar Pradesh, Uttarakhand",
+    location: "Noida",
+    address:
+      "Om Logistics Ltd. Plot No: 63, New Stilt, Sector - 15A Near To Hcl, Noida - 201301 (U.P)",
+    contactPerson: "Mr.H.P.Rana",
+    contactNo: "9990300581",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 23,
+    state: "Uttar Pradesh, Uttarakhand",
+    location: "Lucknow",
+    address:
+      "Om Logistics Ltd. Khasra No-244, Bagiamau, Lekhraj Khazana, Faizabad Road, Near Sai Baba Mandir, Lucknow-226016",
+    contactPerson: "Mr.Brajesh Mishra",
+    contactNo: "7706062462",
+    tollFreeNo: "1800 572 1687",
+  },
+  {
+    index: 24,
+    state: "West Bengal",
+    location: "Kolkata",
+    address:
+      "Om Logistics Ltd Ground Floor, Podder Court, 18, Rabindra Sarani, Gate No. 4, Kolkata - 700001",
+    contactPerson: "Mr.Bimal Chandra",
+    contactNo: "9831278781",
+    tollFreeNo: "1800 572 1687",
+  },
+];
--- src/pages/EwestMgmt/index.jsx
+++ src/pages/EwestMgmt/index.jsx
@@ -1,15 +1,242 @@
 import Head from "@/components/Head";
 import React from "react";
+import {
+  Grid,
+  Typography,
+  Divider,
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableContainer,
+  Box,
+  Card,
+} from "@mui/material";
+import PageHeading from "@/components/PageHeading";
+import { productHeading, modelHeading, contentStyle, cellStyle } from "./style";
+import { benefits, ewasteRules, dos, donts, data } from "helpers/ewasteManage";
+import image from "@/assets/recycle.svg";
 
 const EwestMgmt = () => {
   return (
-    <div>
+    <>
       <Head
         title="E-west Mgmt"
         description="Radhe Enterprise E-west Management Page"
       />
-      EwestMgmt
-    </div>
+
+      <Grid
+        container
+        width={"90%"}
+        sx={{ margin: "2rem auto" }}
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+        rowGap={3}
+      >
+        <Grid item>
+          <Typography sx={productHeading} textAlign={"center"} align="center">
+            E-WASTE MANAGEMENT
+          </Typography>
+        </Grid>
+        <Divider sx={{ width: "100%", backgroundColor: "black" }} />
+
+        <Grid item>
+          <Typography sx={modelHeading}>
+            E-WASTE MANAGEMENT AND HANDLING RULES
+          </Typography>
+          <Typography variant="p" sx={{ lineHeight: "2" }}>
+            {ewasteRules[0]}
+          </Typography>
+        </Grid>
+
+        <Grid container item>
+          <Typography sx={modelHeading}>
+            BENIFITS OF E-WASTE RECYCLING
+          </Typography>
+
+          <Grid
+            container
+            item
+            rowGap={4}
+            columnGap={0}
+            justifyContent={"space-between"}
+          >
+            {benefits?.map((item) => {
+              return item.id === 1 ? (
+                <Grid
+                  container
+                  item
+                  key={item.id}
+                  direction="row"
+                  columnGap={10}
+                  alignItems={"center"}
+                >
+                  <Grid item lg={6} md={5}>
+                    <Typography sx={contentStyle}>{item.title}</Typography>
+                    <Typography sx={{ lineHeight: "2", textAlign: "justify" }}>
+                      {item.content}
+                    </Typography>
+                  </Grid>
+                  <Grid item>
+                    <img src={image} height={300} />
+                  </Grid>
+                </Grid>
+              ) : (
+                <Grid container item key={item.id} lg={5} md={5}>
+                  <Typography sx={contentStyle}>{item.title}</Typography>
+                  <Grid item lg>
+                    <Typography sx={{ lineHeight: "2", textAlign: "justify" }}>
+                      {item.content}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Grid>
+
+        <Grid container item direction="row" columnGap={7}>
+          <Grid item lg={5} md={5}>
+            <Typography sx={modelHeading} align="center">
+              Do's
+            </Typography>
+            {dos.map((item) => {
+              return (
+                <Typography
+                  key={item}
+                  sx={{ lineHeight: "2", textAlign: "justify" }}
+                >
+                  {item}
+                </Typography>
+              );
+            })}
+          </Grid>
+
+          <Grid item lg={6} md={6}>
+            <Typography sx={modelHeading} align="center">
+              Dont's
+            </Typography>
+            {donts.map((item) => {
+              return (
+                <Typography
+                  key={item}
+                  sx={{ lineHeight: "2", textAlign: "justify" }}
+                >
+                  {item}
+                </Typography>
+              );
+            })}
+          </Grid>
+        </Grid>
+
+        <Grid item>
+          <Typography sx={{ ...modelHeading, textAlign: "center" }}>
+            E-WASTE MANAGEMENT PLAN
+          </Typography>
+          <Typography sx={{ lineHeight: "2", textAlign: "justify" }}>
+            In developing country like India, most E-Waste lands up in the
+            informal sector where it is recycled without any consideration to
+            health and environment. We, being responsible citizen contributes
+            our share of Environmental Responsibility and aiding in taking care
+            of our environment. Abiding by all the pertinent laws associated
+            with E-Waste Management, Our Company has been continuously working
+            towards the same. Our company has partnered with PRO namely M/s Kar
+            Parivartan LLP. Together with M/s Kar Parivartan LLP we have been
+            doing its best in order to proffer environmentally sound management
+            of electronic devices which have reached their end- of-life and to
+            comply with the new India E-Waste Management and Handling rules in
+            providing drop-of centers and environmentally sound management of
+            end of life electronics. M/s Kar Parivartan LLP has obtained
+            authorizations from the appropriate governmental agency. M/s Kar
+            Parivartan LLP will receive and recycle customer returned equipment,
+            including all the E-Waste.
+          </Typography>
+        </Grid>
+
+        <Grid item>
+          <Typography sx={{ ...modelHeading, fontSize: "20px" }}>
+            WHERE AND HOW CAN YOU RECYCLE YOUR E-WASTE?
+          </Typography>
+          <Typography sx={{ lineHeight: "2", textAlign: "justify" }}>
+            Our E-Waste program starts with receiving E-Waste pickup request
+            from our end customers through toll free number after which our
+            Authorized PRO Partner collects it and transports it to the
+            collection centre. No fee is charged from the consumer for giving
+            the goods for recycling and there is no monetary benefit included in
+            the Recycling Program. The solo aim is to serve the Environment by
+            keeping it clean. To recycle your waste electronic items, all you
+            have to do is drop E-Waste pick up request by calling us on M/s Kar
+            Parivartan LLP’s Toll Free No. 1800 572 1687 from Monday to Friday
+            between 10:00 AM to 5:00 PM. Also, customers can drop their E-Waste
+            in the drop- box provided at various collection centres of KAR
+            PARIVARTAN LLP.
+          </Typography>
+        </Grid>
+
+        <Typography
+          sx={{ ...modelHeading, textAlign: "center", fontSize: "18px" }}
+        >
+          LIST OF COLLECTION CENTERS
+        </Typography>
+
+        <Card
+          sx={{
+            width: "100%",
+            overflowX: {
+              xs: "scroll",
+              sm: "scroll",
+              lg: "hidden",
+              md: "hidden",
+            },
+          }}
+        >
+          <TableContainer>
+            <Table>
+              <TableHead style={{ backgroundColor: "black", color: "white" }}>
+                <TableRow>
+                  <TableCell sx={cellStyle}>Index</TableCell>
+                  <TableCell sx={cellStyle}>State</TableCell>
+                  <TableCell sx={cellStyle}>Location</TableCell>
+                  <TableCell style={{ width: "19%" }} sx={cellStyle}>
+                    Address
+                  </TableCell>
+                  <TableCell sx={cellStyle}>Contact Person</TableCell>
+                  <TableCell sx={cellStyle}>Contact No</TableCell>
+                  <TableCell style={{ width: "15%" }} sx={cellStyle}>
+                    Toll Free No
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data?.map((item) => {
+                  return (
+                    <TableRow key={item.index}>
+                      <TableCell>{item.index}</TableCell>
+                      <TableCell style={{ width: "15%" }}>
+                        {item.state}
+                      </TableCell>
+                      <TableCell>{item.location}</TableCell>
+                      <TableCell
+                        style={{ width: "19%", wordWrap: "break-word" }}
+                      >
+                        {item.address}
+                      </TableCell>
+                      <TableCell>{item.contactPerson}</TableCell>
+                      <TableCell>{item.contactNo}</TableCell>
+                      <TableCell style={{ width: "15%" }}>
+                        {item.tollFreeNo}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Card>
+      </Grid>
+    </>
   );
 };
 
