2cc1a5fc791301e80f24f6b176672cffcae2374b
hiren2603 <secure.hiren@gmail.com>
2023-06-22 10:28:58 +0530

    re200 plus data added

---
+++ src/helpers/re200PlusData.js
@@ -0,0 +1,136 @@
+export const modal = [
+  {
+    id: 1,
+    modal: "RE200 Plus",
+  },
+];
+
+export const re200PlusPrintParams = [
+  {
+    id: 1,
+    title: "Conduit Length",
+    contet: "3MTR",
+  },
+  {
+    id: 2,
+    title: "Nozzle Specification",
+    contet: "60um",
+  },
+  { id: 3, title: "Printing Height", content: "2mm to 14mm " },
+  { id: 4, title: "Printing Distance", content: "1mm to 15mm" },
+  { id: 5, title: "Printing Lines", content: "1 to 5 Lines" },
+  {
+    id: 6,
+    title: "Printing Speed",
+    content: "450 m/min",
+  },
+  {
+    id: 7,
+    title: "Counter",
+    content: "More than 20 Independent Counter",
+  },
+  {
+    id: 8,
+    title: "Ink Colour",
+    content: "Black, Red, Brown, Royal Blue, Sky Blue, Green",
+  },
+  {
+    id: 9,
+    title: "Font",
+    content:
+      "5x6L, 7x6L, 7x10L, 9x8L, 9×11L, 11x11L, 16x16L, 24×24L, 32×32L, 11x11B, 16×16B, 24×24B, 32×32B",
+  },
+  {
+    id: "10",
+    title: "Printing Material",
+    content:
+      " Carton, Plastic, Metal, Boarding, Pipe,  Stone, Cable, Glass, Electronic components,  Auto parts, Industrial chemical packaging Medical, Food",
+  },
+  {
+    id: 11,
+    title: "Print Content",
+    content:
+      "Text, Time, Date (Julian Year), Logo, Shift, SN, Dynamic Barcode/QR Code, Dynamic Serial Data, Dynamic Database.",
+  },
+];
+
+export const re200PlusParams = [
+  {
+    id: 1,
+    title: "Operating System",
+    content: "Embedded Linux security operating system",
+  },
+  {
+    id: 1,
+    title: "Display Screen",
+    content: "10.1-inch colour display, industrial touchscreen",
+  },
+  {
+    id: 2,
+    title: "Communication Interface",
+    content: "Network port, USB, RS232",
+  },
+  {
+    id: 3,
+    title: "Protection Level",
+    content: "IP55",
+  },
+  {
+    id: 4,
+    title: "Power Parameters",
+    content: "Input 220-240V",
+  },
+  {
+    id: 5,
+    title: "Rated Power",
+    content: "150W",
+  },
+  {
+    id: 6,
+    title: "Memory",
+    content: "256M",
+  },
+  {
+    id: 7,
+    title: "Machine Material",
+    content: "304 stainless steel",
+  },
+  {
+    id: 8,
+    title: "Machine Specifications",
+    content: "447mmx242mmx650mm (L X W X H)",
+  },
+  {
+    id: 9,
+    title: "Working Environment",
+    content: "Temperature0-45°C/Humidity 30-70%Rh",
+  },
+  {
+    id: 10,
+    title: "Related Certification",
+    content: "RoHS. CE. BIS",
+  },
+  {
+    id: 11,
+    title: "Net Weight",
+    content: "30Kg",
+  },
+  {
+    id: 12,
+    title: "Main Hardware",
+    content:
+      "American TIAM3358 industrial grade CPU, American Xilinx Spartan-6 industrial grade FPGA",
+  },
+  {
+    id: 13,
+    title: "External Interface",
+    content:
+      "Optical sensor, Encoder, Reverse and upside down control, Signal light alarm output, Serial number output control, External printing information selection.",
+  },
+  {
+    id: 14,
+    title: "Support Language",
+    content:
+      "Support more than 40 national languages (Chinese, English, Arabic, Danish, German, Russian, French, Finnish, Korean, Japanese, Spanish, Italian, etc).",
+  },
+];
