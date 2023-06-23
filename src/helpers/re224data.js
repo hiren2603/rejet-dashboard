9ab281ef68b766f7ea0f1303d14c6aa781cb74ce
hiren2603 <secure.hiren@gmail.com>
2023-06-23 09:27:59 +0530

    helper files of re200+ and re224 added

---
+++ src/helpers/re224data.js
@@ -0,0 +1,229 @@
+export const re224TechSpec = [
+  {
+    id: 1,
+    label: "Product Model",
+    content: "RE224",
+  },
+  {
+    id: 2,
+    label: "Max printing dots",
+    content: "24 dots",
+  },
+  {
+    id: 3,
+    label: "Printing lines",
+    content: "1-3 lines",
+  },
+  {
+    id: 4,
+    label: "Printing height",
+    content: "1.2-15mm",
+  },
+  {
+    id: 5,
+    label: "Max printing speed",
+    content: "260m/min",
+  },
+];
+
+export const re224FontSpec = [
+  {
+    id: 1,
+    label: "ASCII",
+    content:
+      "3x5, 5x5, 4x6, 5x7, 79.7x13, 8x13, 9x15, 9x18, 10x20, 12×12, 12x24, 16×16, 24×24",
+  },
+  {
+    id: 2,
+    label: "Chinese",
+    content: "12x12, 16×16, 24×24",
+  },
+];
+
+export const re224fileFunctions = [
+  {
+    id: 1,
+    content: "Configure 8 GB memory and store 10000+ files",
+  },
+  {
+    id: 2,
+    content: "Support multi-language custom file name, easy keyword search",
+  },
+];
+
+export const re224Languages = [
+  {
+    id: 1,
+    key: "Interface language",
+    content:
+      "Simplified Chinese. Traditional Chinese. English. Japanese. Korean. Hindi. Italian. Filipino. Turkish. Vietnamese. Spanish French",
+  },
+  {
+    id: 2,
+    key: "User interface",
+    content: "10.1 inch high sensitive TFT touch screen",
+  },
+];
+
+export const re224Software = [
+  {
+    id: 1,
+    featutre: "Support remote online upgrade",
+  },
+  {
+    id: 2,
+    featutre: "Automatic diagnosis of machine status",
+  },
+  {
+    id: 3,
+    featutre: "Parameter backup restore function",
+  },
+  {
+    id: 4,
+    featutre: "Automatically record operation logs",
+  },
+];
+
+export const re224Nozel = [
+  {
+    id: 1,
+    characteristics: "Kotest tie term constant temperature system",
+  },
+  {
+    id: 2,
+    characteristics: "Catheter length 2.5m (optional 5m)",
+  },
+  {
+    id: 3,
+    characteristics: "Minimum bending radius of conduit is 85mm",
+  },
+];
+
+export const re224PrintInfo = [
+  {
+    id: 1,
+    info: "Support all UNICODE content",
+  },
+  {
+    id: 2,
+    info: "Self-edited graphics",
+  },
+  {
+    id: 3,
+    label: "1D barcode",
+    info: "Code39;Code93;Code128;EAN13;ITF;Codebar;UPC_A&C Single line printing 666 characters",
+  },
+  {
+    id: 4,
+    info: "Round-trip printing",
+  },
+  {
+    id: 5,
+    info: "Automatic date update, can print hours, minutes, seconds, date, week, month, year",
+  },
+  {
+    id: 6,
+    info: "Invert images, barcode and text (such as white on black)",
+  },
+  {
+    id: 7,
+    info: "Create and edit messages during coding",
+  },
+  {
+    id: 8,
+    info: "Characters are widened by 14 times",
+  },
+  {
+    id: 9,
+    info: "5 sets of serial numbers can be printed at the same time",
+  },
+];
+
+export const re224Features = [
+  {
+    id: 1,
+    label: "Temperature range",
+    feature: "5 °C - 45 °C (41 F - 113 F)",
+  },
+  {
+    id: 2,
+    label: "Humidity range",
+    feature: "10% -90% RH without condensation",
+  },
+  {
+    id: 3,
+    label: "Power requirement",
+    feature: "AC100-240V,50/60Hz, 150W",
+  },
+  {
+    id: 4,
+    label: "Body material",
+    feature: "carbon steel, black spraying",
+  },
+  {
+    id: 5,
+    label: "Protection level",
+    feature: "IP55",
+  },
+  {
+    id: 6,
+    label: "Dimension",
+    feature: "350°365+445mm",
+  },
+  {
+    id: 7,
+    label: "Weight (without consumables)",
+    feature:
+      "net weight without packaging is about 26.5kg, including the packaging weight of about 27kg.",
+  },
+];
+
+export const re224Benifits = [
+  {
+    id: 1,
+    benifit: "Configure 8 GB memory and store 10000+ files",
+  },
+  {
+    id: 2,
+    benifit: "Support multi-language custom file name, easy keyword search",
+  },
+  {
+    id: 3,
+    benifit: "Unique design and stable operation",
+  },
+  {
+    id: 4,
+    benifit: "Modular ink path design, simple maintenance",
+  },
+  {
+    id: 5,
+    benifit: "The gear pump is durable and doesn't need regular replacement",
+  },
+  {
+    id: 6,
+    benifit: "Automatic fault diagnosis function",
+  },
+  {
+    id: 7,
+    benifit:
+      "10.1-inch touch screen, multi-functional interface, smooth switching, easy to operate",
+  },
+  {
+    id: 8,
+    benifit: "Excellent printing function",
+  },
+  {
+    id: 9,
+    benifit:
+      "The latest patented nozzle technology, Printing fonts clear and neat",
+  },
+  {
+    id: 10,
+    benifit: "Line feed dynamic reverse and rotary jet code",
+  },
+  {
+    id: 11,
+    benifit:
+      "Time automatically updated: hour, minute, second, day, week, month, year",
+  },
+];
