0ba8aaee62730be8829e53cffd34a85d8eaafe0a
hiren2603 <secure.hiren@gmail.com>
2023-06-26 18:29:13 +0530

    re225 data added

---
+++ src/helpers/re225data.js
@@ -0,0 +1,238 @@
+export const re225TechSpec = [
+  {
+    id: 1,
+    label: "Product Model",
+    content: "RE224",
+  },
+  {
+    id: 2,
+    label: "Max printing dots",
+    content: "32 dots",
+  },
+  {
+    id: 3,
+    label: "Printing lines",
+    content: "1-5 lines",
+  },
+  {
+    id: 4,
+    label: "Printing height",
+    content: "1.2-15mm",
+  },
+  {
+    id: 5,
+    label: "Max printing speed",
+    content: "400m/min",
+  },
+  {
+    id: 6,
+    label: "Nozzle size",
+    content: "60um (optional 50um)",
+  },
+];
+
+export const re225DataSpec = [
+  {
+    id: 1,
+    label: "Text",
+    content: "Support all UNICODE content",
+  },
+  {
+    id: 2,
+    label: "Graphics",
+    content: "USB import. ('.bmp' self-edited graphics)",
+  },
+  {
+    id: 3,
+    label: "ID barcode",
+    content: "Code39,Code93,Code128,EAN13, ITF, Barcode, UPC_A&C",
+  },
+  {
+    id: 4,
+    label: "QR Code",
+    content: "QR CODE (Size : 21X21, 25X25, 29X29, 33x33, 37X37)  ",
+  },
+  {
+    id: 5,
+    label: "Data Matrix Size",
+    content:
+      "10X10, 12X12, 14X14, 16x16, 8X18, 18X18, 20X20, 22X22, 24x24, 26x26, 8X32, 32X32, 12x36, 16×36",
+  },
+  {
+    id: 6,
+    label: "Font matrix configuration",
+    content:
+      "3x5, 5x5, 4x6, 5x7, 7X9, 7X13, 8×13, 9x15, 9x18, 10×20, 12X12, 12х24, 16×16, 24×24, 32×32)",
+  },
+];
+
+export const printFunctions = [
+  {
+    id: 1,
+    content: "Single line printing 1000 characters",
+  },
+  {
+    id: 2,
+    content:
+      "Automatic date update, can print hours, minutes, seconds, date, week, month, year",
+  },
+  {
+    id: 3,
+    content: "Invert images, barcode and text (such as white on black)",
+  },
+  {
+    id: 4,
+    content: "Create and edit messages during coding",
+  },
+  {
+    id: 5,
+    content: "5 sets of serial numbers can be printed at the same time",
+  },
+];
+
+export const userInterface = [
+  {
+    id: 1,
+    content: "10.1 inch high sensitive TFT touch screen",
+  },
+  {
+    id: 2,
+    content:
+      "Operating system: CS Printer System i custom developed based on Android",
+  },
+  {
+    id: 3,
+    content: "Operation interface: ACC UI 2.0",
+  },
+];
+
+export const nozzleCharacterities = [
+  {
+    id: 1,
+    content: "Yc-icst intelligent constant temperature system",
+  },
+  {
+    id: 2,
+    content:
+      "Fully automatic ink drop decomposition, charging and controlling ink line",
+  },
+  {
+    id: 3,
+    content: "Catheter length 2.5m (optional 5m)",
+  },
+  {
+    id: 4,
+    content: "Minimum bending radius of conduit is 85 mm",
+  },
+];
+
+export const inkSystem = [
+  {
+    id: 1,
+    label: "Capacity",
+    content: "750ml / 750ml",
+  },
+  {
+    id: 2,
+    label: "Ink colour",
+    content: "black, blue, red, green, brown",
+  },
+];
+
+export const re225Features = [
+  {
+    id: 1,
+    label: "Temperature range",
+    content: "5 °C - 45 °C (41 F - 113 F)",
+  },
+  {
+    id: 2,
+    label: "Humidity range",
+    content: "10% -90% RH without condensation",
+  },
+  {
+    id: 3,
+    label: "Power requirement",
+    content: "AC100-240V,50/60Hz,150W",
+  },
+  {
+    id: 4,
+    label: "Body material",
+    content: "carbon steel, black spraying",
+  },
+  {
+    id: 5,
+    label: "Protection level",
+    content: "IP55",
+  },
+  {
+    id: 6,
+    label: "Dimension",
+    content: "452*261*450mm",
+  },
+  {
+    id: 7,
+    label: "Net weight",
+    content: "",
+  },
+];
+
+export const re225HardwareSpec = [
+  {
+    id: 1,
+    content:
+      "Ink circulation maintenance system (filter system), maintenance interval",
+  },
+  {
+    id: 2,
+    content:
+      "Intelligence temperature maintain system on printhead, suit to the environment automatically",
+  },
+  {
+    id: 3,
+    content:
+      "The ink cartridge and solvent cartridge are designed to prevent incorrect installation, and the capacity can be 750ml",
+  },
+  {
+    id: 4,
+    content: "Support static and dynamic application of nozzle",
+  },
+];
+
+export const re225SoftwareSystem = [
+  {
+    id: 1,
+    content: "One key to start/stop coding",
+  },
+  {
+    id: 2,
+    content: " Automatically save data when power off",
+  },
+  {
+    id: 3,
+    content: "Switch languages without rebooting",
+  },
+  {
+    id: 4,
+    content: "Parameter backup and restore function",
+  },
+  {
+    id: 5,
+    content:
+      "Database files, fonts, pictures, language files, and print files can all be imported or exported through a USB storage device.",
+  },
+  {
+    id: 6,
+    content: "Enhanced Mode Cleaning Nozzles",
+  },
+  {
+    id: 7,
+    content:
+      "One-click automatic cleaning of the entire ink circuit (long-time shutdown)",
+  },
+  {
+    id: 8,
+    content:
+      "Automatically adjusts ink droplets to ensure the best typing quality",
+  },
+];
