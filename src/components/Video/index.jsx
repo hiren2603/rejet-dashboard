776d1e87f1060aaa2f5ec5f5b744c707e66e6a5d
Meet Panchal <meetpanchal2803@gmail.com>
2023-11-01 21:59:55 +0530

    Complete video component in Application page

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -1,63 +1,34 @@
 import { useState, useEffect } from "react";
 import { Box, Skeleton } from "@mui/material";
 import { videoData } from "helpers/videoData";
-import YouTube from "react-youtube";
 
 const Video = () => {
   const [isVideoReady, setIsVideoReady] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  const opts = {
-    height: "200",
-    width: "300",
-    playerVars: { autoplay: 0 },
-  };
 
-  const video = {
-    borderRadius: "20px",
-    margin: "1rem"
-  };
-
-  const handleVideoReady = () => {
-    setIsLoading(false);
-  };
-
-  const handleVideoStateChange = (event) => {
-    if (event.data === YouTube.PlayerState.PLAYING) {
-      setIsVideoReady(true);
-    } else {
-      setIsVideoReady(false);
-    }
-  };
+  const handleVideoReady = () => { setIsVideoReady(true) };
 
   useEffect(() => {
-    setIsLoading(true);
+    setTimeout(handleVideoReady, 2000);
   }, []);
+
   return (
     <>
       {videoData?.map((item) => (
-        <Box
-          key={item.id}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          // sx={{ borderRadius: "5px" }}
-        >
-          <YouTube
-            videoId={item.sourceId}
-            loading="video is loading"
-            opts={opts}
-            style={{
-              borderRadius: "20px",
-              margin: "1rem",
-              display: "none"
-            }}
-            onReady={handleVideoReady}
-            onStateChange={handleVideoStateChange}
+        <Box key={item.id} display={"flex"} alignItems="center" justifyContent={"center"} sx={{ borderRadius: "5px" }}>
+          {!isVideoReady ? (
+            <Skeleton variant="rectangular" height={250} width={300} sx={{ borderRadius: "20px", margin: "1rem" }} />
+          ) : (
+            <iframe
+              width="300"
+              height="250"
+              src={`https://www.youtube.com/embed/${item.sourceId}`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              title="Embedded youtube"
+              style={{ borderRadius: "20px", margin: "1rem" }}
+              onLoad={handleVideoReady}
             />
-          {isLoading && !isVideoReady ? (
-              <Skeleton variant="rectangular" height={250} width={300} />
-            ) : null
-          }
+          )}
         </Box>
       ))}
     </>
