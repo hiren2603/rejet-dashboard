7832fccc249c5169d55260fa6ce081c80fe0372e
Meet Panchal <meetpanchal2803@gmail.com>
2023-11-01 22:36:14 +0530

    Add loader for app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import {
   About,
@@ -16,7 +16,7 @@ import {
   TpOne,
   EwestMgmt,
 } from "@/pages";
-import { ThemeProvider, CssBaseline } from "@mui/material";
+import { ThemeProvider, CssBaseline, Box, CircularProgress } from "@mui/material";
 import { Layout } from "@/components/Shared";
 import { theme } from "@/theme";
 import ErrorPage from "@/pages/ErrorPage";
@@ -24,41 +24,64 @@ import { LayoutContext } from "@/Context";
 import { HelmetProvider } from "react-helmet-async";
 
 const App = () => {
+  const helmetContext = {};
   const [drawerwidth] = useState(200);
   const [open, setOpen] = useState(false);
-  const helmetContext = {};
-  return (
-    <HelmetProvider context={helmetContext}>
-      <ThemeProvider theme={theme}>
-        <LayoutContext.Provider value={{ open, setOpen, drawerwidth }}>
-          <CssBaseline />
-          <Layout>
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route exact path="/about" element={<About />} />
-              <Route>
-                {/* <Route exact path="/products/" element={<Navigate replace to="/" />} /> */}
-                <Route exact path="/products/inkjet/re200+" element={<Re200plus />} />
-                <Route exact path="/products/inkjet/re224" element={<Re224 />} />
-                <Route exact path="/products/inkjet/re225" element={<Re225 />} />
-                <Route exact path="/products/laser/co2" element={<Co2 />} />
-                <Route exact path="/products/laser/fiber" element={<Fiber />} />
-                <Route exact path="/products/laser/uv" element={<Uv />} />
-                <Route exact path="/products/laser/desktop" element={<Desktop />} />
-                <Route exact path="/products/tij/1" element={<TpOne />} />
-                {/* <Route path="/products/tij/tp01.1" element={<TpOneOne />} /> */}
-                <Route exact path="/products/tij/handy" element={<TpHandy />} />
-              </Route>
-              <Route exact path="/applications" element={<Applications />} />
-              <Route exact path="/ewest-management" element={<EwestMgmt />} />
-              <Route exact path="/contact" element={<Contact />} />
-              <Route path="*" element={<ErrorPage />} />
-            </Routes>
-          </Layout>
-        </LayoutContext.Provider>
-      </ThemeProvider>
-    </HelmetProvider>
-  );
+  const [isComponentReady, setIsComponentReady] = useState(false);
+
+  const handleComponentReady = () => { setIsComponentReady(true) };
+
+  const LoaderStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+  }
+
+  useEffect(() => {
+    setTimeout(handleComponentReady, 2000);
+  }, []);
+
+  if (!isComponentReady) {
+    return (
+      <Box sx={LoaderStyle} >
+        <CircularProgress color="error" size={80} />
+      </Box>
+    )
+  } else {
+    return (
+      <HelmetProvider context={helmetContext}>
+        <ThemeProvider theme={theme}>
+          <LayoutContext.Provider value={{ open, setOpen, drawerwidth }}>
+            <CssBaseline />
+            <Layout>
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/about" element={<About />} />
+                <Route>
+                  {/* <Route exact path="/products/" element={<Navigate replace to="/" />} /> */}
+                  <Route exact path="/products/inkjet/re200+" element={<Re200plus />} />
+                  <Route exact path="/products/inkjet/re224" element={<Re224 />} />
+                  <Route exact path="/products/inkjet/re225" element={<Re225 />} />
+                  <Route exact path="/products/laser/co2" element={<Co2 />} />
+                  <Route exact path="/products/laser/fiber" element={<Fiber />} />
+                  <Route exact path="/products/laser/uv" element={<Uv />} />
+                  <Route exact path="/products/laser/desktop" element={<Desktop />} />
+                  <Route exact path="/products/tij/1" element={<TpOne />} />
+                  {/* <Route path="/products/tij/tp01.1" element={<TpOneOne />} /> */}
+                  <Route exact path="/products/tij/handy" element={<TpHandy />} />
+                </Route>
+                <Route exact path="/applications" element={<Applications />} />
+                <Route exact path="/ewest-management" element={<EwestMgmt />} />
+                <Route exact path="/contact" element={<Contact />} />
+                <Route path="*" element={<ErrorPage />} />
+              </Routes>
+            </Layout>
+          </LayoutContext.Provider>
+        </ThemeProvider>
+      </HelmetProvider>
+    )
+  }
 };
 
 export default App;
