585c21096f78bc8713804f9f2fb9e24091528961
hiren2603 <secure.hiren@gmail.com>
2023-11-03 23:01:33 +0530

    removed unwanted files

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,16 +1,20 @@
 import { useEffect } from "react";
-import { redirect } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import ErrorImage  from "../../assets/404.svg"
 import { Grid, Typography } from "@mui/material";
 
 const ErrorPage = () => {
+  const navigate = useNavigate()
   useEffect(() => {
-    redirect("/");
-  });
+    const timer = setTimeout(()=>{
+      navigate("/");
+    }, 1000)
+    return ()=> clearTimeout(timer)
+  },[]);
 
   return (
     <Grid container justifyContent="center" alignItems="center" >
-        <Typography variant="h4" sx={{fontWeight: 600, color: "#FF725E"}}>Error!! Page Not Found!</Typography>
+        <Typography variant="h4" sx={{fontWeight: 600, color: "#FF725E"}}>Opps!! Page Not Found!</Typography>
       <Grid item lg={12} sx={{ display: "flex", justifyContent: "center", alignItems: "center", mb: 2}}>
         <img src={ErrorImage} alt="error" style={{width: "60%"}}/>
       </Grid>
